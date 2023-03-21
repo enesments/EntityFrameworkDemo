e8e5504e790523069c4573d6bd8bacb7248a481e
enesments <enesments@gmail.com>
2023-03-21 13:43:16 +0300

    Add project files.

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityFrameworkDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ADO.NET
+            //EntityFrameworkDemo --Bir ORM - Object relational mapping
+            //NHibernate
+            //GetAll();
+            GetProductsByCategory(1);
+
+
+
+        }
+
+        private static void GetAll()
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+            foreach (var product in northwindContext.Products)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+        private static void GetProductsByCategory(int categoryId)
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+            var result = northwindContext.Products.Where(p => p.CategoryId == categoryId);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+    }
